e07bb8329c44e54913761cb5832d0a44dd3fc61f
+ <+>
2023-07-30 15:20:11 +0300

    updated the army

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       <RobotsDisplay 
       displays = {displays} 
       setDisplays = {setDisplays} 
+      favoriteBots = {favoriteBots}
       setFavoriteBots = {setFavoriteBots}
       />
     </div>
