237376291cae595496968e95ccb9cc4972d16626
+ <+>
2023-07-30 08:16:09 +0300

    fetched data and displayed favorite robots

--- src/RobotsDisplay.js
+++ src/RobotsDisplay.js
@@ -11,10 +11,20 @@ useEffect(() =>  {fetch ('http://localhost:3000/bots')
 }, [])
 
 const robots = displays.map((display) => 
-<li key={display.id}>{display.name}</li>
+<div key={display.id} className = "card col-3 m-1"  >   
+  <img src = {display.avatar_url} className ="card-img-top" alt = {display.name}/>
+  <div className ="card-body">
+    <h5 className ="card-title">{display.name}</h5>
+    <p className ="card-text">{display.health}</p>
+   
+  </div>
+  </div>
+ 
+
+
 )
     return (
-        <div>
+        <div className="row">
             {robots}
         </div>
     )
